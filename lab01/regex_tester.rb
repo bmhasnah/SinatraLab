3573cbdd5eddf23132847b4e720a547b35de07bd
bmhasnah <bmhasnah@qatar.cmu.edu>
2017-01-30 10:54:33 +0000

    sinatra lab

--- lab01/regex_tester.rb
+++ lab01/regex_tester.rb
@@ -7,23 +7,25 @@ class RegexTester
     attr_accessor :pattern 
     
     def statements=(arr)
-        begin
-            raise TypeError unless arr.class == Array 
-            raise RuntimeError if arr.empty? @statements = arr
-            rescue RuntimeError
-            STDERR.puts "You need to have at least one statement to test against the pattern." 
-            add_insult
-            exit
-            rescue TypeError
-            STDERR.puts "You must enter an ARRAY of statements to use this regex_tester."
-            add_insult
-            exit
-        end 
+       begin
+        raise TypeError unless arr.class == Array 
+        raise RuntimeError if arr.empty?
+        @statements = arr
+        rescue RuntimeError
+        STDERR.puts "You need to have at least one statement to test against the pattern." 
+        add_insult
+        exit
+        rescue TypeError
+        STDERR.puts "You must enter an ARRAY of statements to use this regex_tester." 
+        add_insult
+        exit
+    end
+
     end
     
     def add_insult
         STDERR.puts "INSULT" 
-        STDERR.puts "As a coding infidel, you are hereby sentenced to death. The firing squad will be here shortly to carry out the execution. Please remain seated until they arrive. Thank you for your cooperation."
+       # STDERR.puts "As a coding infidel, you are hereby sentenced to death. The firing squad will be here shortly to carry out the execution. Please remain seated until they arrive. Thank you for your cooperation."
     end
 
     def statements
@@ -31,26 +33,40 @@ class RegexTester
     end 
     
     def test
-        if pattern_matches? @statements
-        puts "MATCH: #{@statements}" else
-        STDERR.puts "NO MATCH: #{@statements}" end
+        @statements.each do |r|
+            if pattern_matches? r
+             puts "MATCH: #{r}" else
+            STDERR.puts "NO MATCH: #{r}" end
+        end
     end
     
-    #private
+    private
     def  pattern_matches? statements
      statements.match(@pattern) != nil
-     end 
+    end 
     
 end
 
 regex = RegexTester.new
-regex.pattern = /^(http:\/\/)?www\.\w+\.(com|edu|org)$/ # from test_arrays.rb puts regex.pattern
-regex.statements = "http://www.google.com"
+#cc.statements= %w[1234567890123456 1234-5678-9012-3456 1234\ 5678\ 9012\ 3456
+#1234567890 #1234567890123456 1234|5678|9012|3456]
+#cc.pattern= /^\d{16}$/
+#cc.pattern = /^\d{16}$|^(\d{4}-\d{4}-\d{4}-\d{4})$/
+#cc.pattern = /^\d{16}$|^((\d{4}-){4})$/
+#cc.pattern = /^\d{16}$|^((\d{4}-?){4})$/
+#cc.pattern = /^\d{16}$|^(\d{4}[ -]?){4}$/
+#cc.pattern = /^(\d{4}[ -]?){4}$/
+#cc.test
+#=begin
+puts "------1"
+regex.pattern = /^\S\.?\S+\.(com|edu|org|net|co|uk)$/ # from test_arrays.rb puts regex.pattern
+puts "------2"
+regex.statements = ['http://www.google.com', 'apidock.com', 'www.microsoft.com', 'http://www.heimann-family.org', 'http://www.kli.org', 'http://www.acac.net', 'http://www.cmu.edu', 'http://is.hss.cmu.edu', 'www.amazon.co.uk']
+puts "------3"
 puts regex.statements
-puts "------"
+puts "------4"
 regex.test
 regex.statements = "apidock.com"
 regex.test
-
-
+#=end
 
